f9a09178b65688da9c898233c99214b06030e50c
mubeen207 <mubeenulislam207@gmail.com>
2025-09-16 01:07:32 +0500

    Completed PErfectly

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 let myInput = document.getElementById("myInput");
 let myTable = document.getElementById("myTable");
-let tbody = myTable.querySelector("tbody");
 let name1 = document.getElementById("name1");
 let country = document.getElementById("country");
+let tbody = myTable.querySelector("tbody");
+let tr = tbody.getElementsByTagName("tr");
 let cityName = "";
 let countryName = "";
-let found = true;
+let foundedCity;
+let foundedCountry;
 let start = true;
-let tbodyChild;
 
 function myFunction() {
   let found = false;
@@ -80,32 +81,47 @@ function borderReset() {
   name1.style.border = "0.5px solid black";
   country.style.border = "0.5px solid black";
 }
-function recheak() {
-  found = true;
-  tbodyChild = tbody.childNodes;
-  for (let i = 0; i < tbodyChild.length; i++) {
-    if (tbodyChild[i].nodeType === 1 && found) {
-      for (let j = 0; j < tbodyChild[i].childNodes.length; j++) {
-        if (tbodyChild[i].childNodes[j].nodeType === 1 && found) {
-          if (
-            name1.value.toLowerCase() ===
-              tbodyChild[i].childNodes[j].innerHTML.toLowerCase() ||
-            country.value.toLowerCase() ===
-              tbodyChild[i].childNodes[j].innerHTML.toLowerCase()
-          ) {
-            if (
-              country.value.toLowerCase() !==
-              tbodyChild[i].childNodes[j].innerHTML.toLowerCase()
-            ) {
-              alert("Dublicate City Entry Not Allowed");
+function recheak(start) {
+  for (let i = 0; i < tr.length; i++) {
+    foundedCity = tr[i].children[0].innerHTML.toLowerCase();
+    foundedCountry = tr[i].children[1].innerHTML.toLowerCase();
 
-              found = false;
-              start = false;
-              return false;
-            }
-          }
-        }
-      }
+    if (name1.value.toLowerCase() === foundedCountry) {
+      alert("Your Enterd City Name is a Country Name Only City Name Allowed");
+      console.log("Hello Country");
+      return (start = false);
+    } else if (name1.value.toLowerCase() === foundedCity) {
+      alert("Duplicate City Not Allowed");
+      return (start = false);
     }
   }
 }
+// function recheak() {
+//   found = true;
+//   tbodyChild = tbody.childNodes;
+//   for (let i = 0; i < tbodyChild.length; i++) {
+//     if (tbodyChild[i].nodeType === 1 && found) {
+//       for (let j = 0; j < tbodyChild[i].childNodes.length; j++) {
+//         if (tbodyChild[i].childNodes[j].nodeType === 1 && found) {
+//           if (
+//             name1.value.toLowerCase() ===
+//               tbodyChild[i].childNodes[j].innerHTML.toLowerCase() ||
+//             country.value.toLowerCase() ===
+//               tbodyChild[i].childNodes[j].innerHTML.toLowerCase()
+//           ) {
+//             if (
+//               country.value.toLowerCase() !==
+//               tbodyChild[i].childNodes[j].innerHTML.toLowerCase()
+//             ) {
+//               alert("Dublicate City Entry Not Allowed");
+
+//               found = false;
+//               start = false;
+//               return false;
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
