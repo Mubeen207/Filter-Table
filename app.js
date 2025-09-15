8ea55471849506aa43f6e32ca12a5e82bdb8cf1d
mubeen207 <mubeenulislam207@gmail.com>
2025-09-15 23:51:21 +0500

    Doblecate Addind Function Added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let myInput = document.getElementById("myInput");
 let myTable = document.getElementById("myTable");
 let tbody = myTable.querySelector("tbody");
-
 let name1 = document.getElementById("name1");
 let country = document.getElementById("country");
 let cityName = "";
 let countryName = "";
+let found = true;
+let start = true;
 
 function myFunction() {
   let found = false;
@@ -40,36 +41,69 @@ function myFunction() {
   }
 }
 function add() {
-  cityName = name1.value;
-  countryName = country.value;
-  cityName =
-    cityName.slice(0, 1).toUpperCase() + cityName.slice(1).toLowerCase();
-  countryName =
-    countryName.slice(0, 1).toUpperCase() + countryName.slice(1).toLowerCase();
-  let addSetionTr = document.createElement("tr");
-  let addSetionTd1 = document.createElement("td");
-  let addSetionTd2 = document.createElement("td");
-  if (name1.value === "" && country.value === "") {
-    name1.style.border = "0.5px solid red";
-    country.style.border = "0.5px solid red";
-  } else if (name1.value === "") {
-    name1.style.border = "0.5px solid red";
-  } else if (country.value === "") {
-    country.style.border = "0.5px solid red";
-  } else if (name1.value !== "" && country.value !== "") {
-    console.log(cityName);
-    console.log(countryName);
+  recheak(start);
+  if (start) {
+    cityName = name1.value;
+    countryName = country.value;
+    cityName =
+      cityName.slice(0, 1).toUpperCase() + cityName.slice(1).toLowerCase();
+    countryName =
+      countryName.slice(0, 1).toUpperCase() +
+      countryName.slice(1).toLowerCase();
+    let addSetionTr = document.createElement("tr");
+    let addSetionTd1 = document.createElement("td");
+    let addSetionTd2 = document.createElement("td");
+    if (name1.value === "" && country.value === "") {
+      name1.style.border = "0.5px solid red";
+      country.style.border = "0.5px solid red";
+    } else if (name1.value === "") {
+      name1.style.border = "0.5px solid red";
+    } else if (country.value === "") {
+      country.style.border = "0.5px solid red";
+    } else if (name1.value !== "" && country.value !== "") {
+      // console.log(cityName);
+      // console.log(countryName);
 
-    tbody.appendChild(addSetionTr);
-    addSetionTr.appendChild(addSetionTd1);
-    addSetionTr.appendChild(addSetionTd2);
-    addSetionTd1.innerHTML = cityName;
-    addSetionTd2.innerHTML = countryName;
+      tbody.appendChild(addSetionTr);
+      addSetionTr.appendChild(addSetionTd1);
+      addSetionTr.appendChild(addSetionTd2);
+      addSetionTd1.innerHTML = cityName;
+      addSetionTd2.innerHTML = countryName;
+    }
+    name1.value = "";
+    country.value = "";
   }
-  name1.value = "";
-  country.value = "";
+  start = true;
 }
 function borderReset() {
   name1.style.border = "0.5px solid black";
   country.style.border = "0.5px solid black";
+}
+function recheak() {
+  found = true;
+  for (let i = 0; i < tbody.childNodes.length; i++) {
+    if (tbody.childNodes[i].nodeType === 1 && found) {
+      for (let j = 0; j < tbody.childNodes[i].childNodes.length; j++) {
+        if (tbody.childNodes[i].childNodes[j].nodeType === 1 && found) {
+          if (
+            name1.value.toLowerCase() ===
+              tbody.childNodes[i].childNodes[j].innerHTML.toLowerCase() ||
+            country.value.toLowerCase() ===
+              tbody.childNodes[i].childNodes[j].innerHTML.toLowerCase()
+          ) {
+            if (
+              country.value.toLowerCase() !==
+              tbody.childNodes[i].childNodes[j].innerHTML.toLowerCase()
+            ) {
+              alert("Dublicate City Entry Not Allowed");
+
+              found = false;
+              start = false;
+              return false;
+            }
+          }
+        }
+      }
+    }
+  }
 }
